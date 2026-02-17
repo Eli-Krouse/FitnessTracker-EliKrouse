ce56201159865bea2b8e670793bb1757e9db9605
Eli-Krouse <williamelijahkrouse@gmail.com>
2026-02-17 11:15:56 -0500

    Initial Commit

---
+++ FitnessTracker-EliKrouse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessTracker-EliKrouse
+//
+//  Created by Krouse, William E. on 2/17/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
